4aa0073756f4468a815126008fd98e01df2e0815
DevDimonLocal <devdimonpok@gmail.com>
2022-12-11 10:36:11 +0300

    Result 1_v1_1

---
+++ lab1v1_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(void)
+{
+    float Ax, Ay, Bx, By, AB;
+    cout << "Координаты точки A" << std::endl;
+    cout << "    x = ";
+    cin >> Ax;
+    cout << "    y = ";
+    cin >> Ay;
+    cout << "Координаты точки B" << std::endl;
+    cout << "    x = ";
+    cin >> Bx;
+    cout << "    y = ";
+    cin >> By;
+    AB = sqrt(pow(Bx - Ax, 2) + pow(By - Ay, 2));
+    cout << "Длина отрезка AB =" << AB;
+}
