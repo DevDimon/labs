9a408e58ce71fcc8abbe6fc8201c29ecc3c16e58
DevDimonLocal <devdimonpok@gmail.com>
2023-04-02 20:16:33 +0300

    Result 5_2

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -6,5 +6,29 @@ using namespace std;
 
 int main(void)
 {
-    
+    const int N = 10;
+    int i;
+    double Value[N];
+
+    double avgLen = 0; 
+    int countLen = 0;
+
+    cout << "Введите рост:\n";
+    for (i = 0; i < N; i++)
+    {
+        cout << "Ученик " << (i+1) << " -> ";
+        cin >> Value[i];
+        avgLen+=Value[i];
+    }
+
+    avgLen = avgLen / N;
+
+    for (i = 0; i < N; i++)
+    {
+        if (Value[i] > avgLen)
+        {
+            countLen++;
+        }
+    }
+   cout << "Количество учеников, чей рост превышает средний (" << avgLen << ") равно " << countLen; 
 }
