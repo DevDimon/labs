46cd8086ad9d966eec8a40cab05a62bd5e5654bb
DevDimonLocal <devdimonpok@gmail.com>
2023-04-04 20:10:03 +0300

    Result 5_4

--- lab5_4.cpp
+++ lab5_4.cpp
@@ -7,21 +7,42 @@ using namespace std;
 int main(void)
 {
     const int N = 10;
-    int i, j;
-    double Value[N];
+    int i, j, count = 0;
+    double Value[N], Check[N];
 
     cout << "Введите число:\n";
     for (i = 0; i < N; i++)
     {
-        cout << "Число " << (i+1) << " -> ";
+        cout << "Число " << (i + 1) << " -> ";
         cin >> Value[i];
+        Check[i] = 0;
     }
 
     for (i = 0; i < N; i++)
     {
-        for (j = i+1; j < N; j++)
+        count = 0;
+        if (Check[i] == 0)
         {
-
+            for (j = i + 1; j < N; j++)
+            {
+                if (Value[i] == Value[j])
+                {
+                    Check[j] = 1;
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Check[i] = 1;
+            }
+        }
+    }
+    
+    for (i = 0; i < N; i++)
+    {
+        if (Check[i] == 0)
+        {
+            cout << Value[i] << "  ";
         }
     }
 }
